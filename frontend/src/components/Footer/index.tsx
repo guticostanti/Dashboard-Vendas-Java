bdf46aaa6e271630244df8c6be8dc6dd3f12a9b4
Guti <gustavo.costanti.lara@gmail.com>
2021-05-03 16:34:13 -0300

    03 - Basic Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/guticostanti" target="_blank" rel="noreferrer">Gustavo Costanti Lara</a></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
