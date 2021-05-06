d9b8b0c94934bdcc1585ad6546ee32d86a8fd7a8
Guti <gustavo.costanti.lara@gmail.com>
2021-05-05 23:30:02 -0300

    09 - group by query

--- backend/src/main/java/com/guticostanti/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/guticostanti/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.guticostanti.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.guticostanti.dsvendas.dto.SaleDTO;
+import com.guticostanti.dsvendas.dto.SaleSuccessDTO;
+import com.guticostanti.dsvendas.dto.SaleSumDTO;
 import com.guticostanti.dsvendas.entities.Sale;
 import com.guticostanti.dsvendas.repositories.SaleRepository;
 import com.guticostanti.dsvendas.repositories.SellerRepository;
@@ -26,4 +30,15 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
+	}
+	
 }
